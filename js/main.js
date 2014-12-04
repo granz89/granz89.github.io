f8d8c600dd2e54204d0cfe016c4ec5f846b022dd
granz89 <a.granieri@hotmail.com>
2014-12-04 12:16:31 -0500

    fixed things

--- js/main.js
+++ js/main.js
@@ -23,3 +23,20 @@ $designerSection.waypoint(function () {
 	$designer.addClass('js-designer-fade');
 }, { offset: '50%' });
 
+
+//smooth scrolling///
+
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
